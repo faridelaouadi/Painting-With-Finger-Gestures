30a2da9345209fdd12dd6effc4a77c6f5bb438bd
Farid El-Aouadi <faridelaouadi@hotmail.co.uk>
2019-01-08 14:44:51 +0000

    cleaning the code

--- finger_painting.py
+++ finger_painting.py
@@ -5,57 +5,59 @@ from collections import deque
 
 #painting stuff inspired by : #https://github.com/akshaychandra21/Webcam_Paint_OpenCV
 
-'''
-To Do:
-- make colours a dictionary
-'''
-
-'''
-Program that does the following:
-- thresholds the hand
-- draws contour and convex hull around the hand
-- draws purple dot in the centre of hand
-- draws light blue at fingertip
-- tracks the fingertip and paints where it goes
-'''
-
-cap = cv2.VideoCapture(0)
-cv2.namedWindow('output',cv2.WINDOW_NORMAL)
+webcam = cv2.VideoCapture(0) #accessing the webcam
+cv2.namedWindow('output',cv2.WINDOW_NORMAL) #creating the window
 cv2.resizeWindow('output', 600,600) #resizing the window
 
-# Setup deques to store separate colors in separate arrays
+
+
+# Setup deques to store separate colours in separate arrays
 bpoints = [deque(maxlen=512)]
 gpoints = [deque(maxlen=512)]
 rpoints = [deque(maxlen=512)]
 ypoints = [deque(maxlen=512)]
 
+#these indexes allow me to know what queue the current live painting should go into
 bindex = 0
 gindex = 0
 rindex = 0
 yindex = 0
 
-colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
-colorIndex = 0
+
+colours = {"BLUE":(255, 0, 0), "GREEN":(0, 255, 0),"RED": (0, 0, 255), "YELLOW":(0, 255, 255)}
+current_colour = "BLUE" #default colour is blue
 user_currently_painting = False
 
-# Setup the Paint interface
+
+
+'''
+*******************
+*******************
+Setting up the painting canvas
+*******************
+*******************
+'''
 paintWindow = np.zeros((471,636,3)) + 255 #the white canvas for the paint application
+#placing the rectangles for each colour at the top left
+#cv2.rectangle(where_to_place_rectangle, top_left_corner, bottom_right_corner, colour, borderWidth)
 paintWindow = cv2.rectangle(paintWindow, (40,1 ), (140,65), (0,0,0), 2)
-paintWindow = cv2.rectangle(paintWindow, (160,1), (255,65), colors[0], -1)
-paintWindow = cv2.rectangle(paintWindow, (275,1), (370,65), colors[1], -1)
-paintWindow = cv2.rectangle(paintWindow, (390,1), (485,65), colors[2], -1)
-paintWindow = cv2.rectangle(paintWindow, (505,1), (600,65), colors[3], -1)
+paintWindow = cv2.rectangle(paintWindow, (160,1), (255,65), colours["BLUE"], -1) #negative border width => fill
+paintWindow = cv2.rectangle(paintWindow, (275,1), (370,65), colours["GREEN"], -1)
+paintWindow = cv2.rectangle(paintWindow, (390,1), (485,65), colours["RED"], -1)
+paintWindow = cv2.rectangle(paintWindow, (505,1), (600,65), colours["YELLOW"], -1)
 cv2.putText(paintWindow, "CLEAR ALL", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
 cv2.putText(paintWindow, "BLUE", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 cv2.putText(paintWindow, "GREEN", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 cv2.putText(paintWindow, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 cv2.putText(paintWindow, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (150,150,150), 2, cv2.LINE_AA)
 
+
 def printThreshold(thr):
     #this function is only used in the trackbar threshold thingy
     print("! Changed threshold to "+str(thr))
-
+#function that finds the centre of mass of given contour
 def centroid(largest_contour):
+
     moment = cv2.moments(largest_contour)
     if moment['m00'] != 0:
         cx = int(moment['m10'] / moment['m00'])
@@ -63,7 +65,7 @@ def centroid(largest_contour):
         return cx, cy
     else:
         return None
-
+#finds the furthest convexity defect from the centre of mass of a contour
 def farthest_point(defects, contour, centroid):
     if defects is not None and centroid is not None:
         #if you have list of defects and a centre of hand
@@ -87,49 +89,20 @@ def farthest_point(defects, contour, centroid):
             return None
     else:
         return None
-
-cv2.namedWindow('trackbar')
-cv2.createTrackbar('trh1', 'trackbar', 0, 100, printThreshold)
-
-while True:
-
-    _, frame = cap.read()
-
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    threshold = cv2.getTrackbarPos('trh1', 'trackbar') #the value from the trackbar
-
-    lower_skin = np.array([0,37,0])
-    upper_skin = np.array([17,154,255])
-
-    mask = cv2.inRange(hsv, lower_skin, upper_skin)
-    res = cv2.bitwise_and(frame,frame, mask= mask)
-    kernel = np.ones((15,15),np.float32)/225
-    smoothed = cv2.filter2D(res,-1,kernel)
-    median = cv2.medianBlur(res,15)
-    #cv2.imshow('Median Blur',median)
-
-    gray = cv2.cvtColor(cv2.cvtColor(median, cv2.COLOR_HSV2BGR), cv2.COLOR_BGR2GRAY) #converting the median blurred image to grayscale
-    #cv2.imshow("gray", gray)
-
-    ret, thresh = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)
-    _, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    #contours is the list of numpy arrays of the countours found
-    #cv2.imshow('threshold', thresh)
-
-
+def what_menu_button_pressed(fingerTip_x):
+    if 40 <= fingerTip_x <= 140: # user selecting CLEAR ALL
+        return "CLEAR ALL"
+    elif 160 <= fingerTip_x <= 255:
+        return "BLUE"
+    elif 275 <= fingerTip_x <= 370:
+        return "GREEN"
+    elif 390 <= fingerTip_x <= 485:
+        return "RED"
+    elif 505 <= fingerTip_x <= 600:
+        return "YELLOW"
+def find_largest_contour(contours):
     number_of_contours = len(contours)
     largest_contour_area =  -1
-    canvas = np.zeros(median.shape, np.uint8) #an empty canvas to draw on
-    canvas = cv2.rectangle(canvas, (40,1), (140,65), (122,122,122), -1) #topleft, bottomRight, borderColour, -1 ==> fill rectangle
-    canvas = cv2.rectangle(canvas, (160,1), (255,65), colors[0], -1)
-    canvas = cv2.rectangle(canvas, (275,1), (370,65), colors[1], -1)
-    canvas = cv2.rectangle(canvas, (390,1), (485,65), colors[2], -1)
-    canvas = cv2.rectangle(canvas, (505,1), (600,65), colors[3], -1)
-    cv2.putText(canvas, "CLEAR ALL", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
-    cv2.putText(canvas, "BLUE", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
-    cv2.putText(canvas, "GREEN", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
-    cv2.putText(canvas, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
-    cv2.putText(canvas, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (150,150,150), 2, cv2.LINE_AA)
     if number_of_contours > 0: #if you have found atleast 1 countours
         #loop through the contours and find the largest one which we consider is the hand
         for i in range(number_of_contours):
@@ -139,13 +112,77 @@ while True:
                 index_of_largest_contour = i
 
         largest_contour = contours[index_of_largest_contour]
-        #largest_contour is the hand
+        return largest_contour
+    else:
+        return None
+def draw_dot_between_fingers(defects,largest_contour,canvas):
+    for i in range(defects.shape[0]):
+        s,e,f,d = defects[i,0]
+        start = tuple(largest_contour[s][0])
+        end = tuple(largest_contour[e][0])
+        far = tuple(largest_contour[f][0])
+        # cosine theorem
+        a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
+        b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
+        c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
+        angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))
+        if angle <= math.pi / 2:  # angle less than 90 degree, treat as fingers
+            #number_of_fingers += 1
+            cv2.circle(canvas,far,10,[255,0,0],-1) #find the fingers
+
+#creating the trackbar window
+cv2.namedWindow('trackbar')
+cv2.createTrackbar('trh1', 'trackbar', 0, 200, printThreshold)
+
+while True:
+
+    _, frame = webcam.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    threshold = cv2.getTrackbarPos('trh1', 'trackbar') #the value from the trackbar
+
+    lower_skin = np.array([0,37,0])
+    upper_skin = np.array([17,154,255])
+
+    mask = cv2.inRange(hsv, lower_skin, upper_skin) #masks everything so that only the "Skin" is left
+    binary_image = cv2.bitwise_and(frame,frame, mask= mask) #makes the skin white and everything else black
+    #more info on the kernal and smoothed can be found here : https://github.com/atduskgreg/opencv-processing-book/blob/master/book/filters/blur.md
+    kernel = np.ones((15,15),np.float32)/225
+    smoothed = cv2.filter2D(binary_image,-1,kernel)
+    median = cv2.medianBlur(binary_image,15)
+
+    gray = cv2.cvtColor(cv2.cvtColor(median, cv2.COLOR_HSV2BGR), cv2.COLOR_BGR2GRAY) #converting the median blurred image to grayscale
+
+    ret, thresh = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)
+    _, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #contours is the list of numpy arrays of the countours found
+
+    '''
+    *******************
+    *******************
+    Setting up the frame to draw the hand on
+    *******************
+    *******************
+    '''
+    canvas = np.zeros(median.shape, np.uint8) #an empty canvas to draw on same size as the median image
+    canvas = cv2.rectangle(canvas, (40,1), (140,65), (122,122,122), -1)
+    canvas = cv2.rectangle(canvas, (160,1), (255,65), colours["BLUE"], -1)
+    canvas = cv2.rectangle(canvas, (275,1), (370,65), colours["GREEN"], -1)
+    canvas = cv2.rectangle(canvas, (390,1), (485,65), colours["RED"], -1)
+    canvas = cv2.rectangle(canvas, (505,1), (600,65), colours["YELLOW"], -1)
+    cv2.putText(canvas, "CLEAR ALL", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(canvas, "BLUE", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(canvas, "GREEN", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(canvas, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(canvas, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (150,150,150), 2, cv2.LINE_AA)
+
+        largest_contour = find_largest_contour(contours)
+        #largest_contour is taken to be the hand
         centre_of_hand = centroid(largest_contour)
         #centre_of_hand is the (x,y) of the centre of the hand
-
-        cv2.drawContours(canvas, [largest_contour], 0, (0, 255, 0), 3) #thickness=cv2.FILLED
+        cv2.drawContours(canvas, [largest_contour], 0, (0, 255, 0), 3)
         #draw the hand
-        #cv2.drawContours(canvas, [cv2.convexHull(largest_contour)], 0, (0, 0, 255), 3)
+        cv2.drawContours(canvas, [cv2.convexHull(largest_contour)], 0, (0, 0, 255), 3)
         #draw convex hull around hand
         cv2.circle(canvas, centre_of_hand, 5, [255, 0, 255], -1)
         #draws a purple dot at the centre of the hand
@@ -154,35 +191,21 @@ while True:
         defects = cv2.convexityDefects(largest_contour,hull)
         #two lines above are to find convexity defects
 
-        if type(defects) != type(None):#this if statement finds the base of the fingers
+
+        if type(defects) != type(None): #if there are defects
             #if there are some defects
-
-            number_of_fingers = 0
             #https://goo.gl/dkYhLs --> code below is from here
-            for i in range(defects.shape[0]):
-                s,e,f,d = defects[i,0]
-                start = tuple(largest_contour[s][0])
-                end = tuple(largest_contour[e][0])
-                far = tuple(largest_contour[f][0])
-                # cosine theorem
-                a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
-                b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
-                c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
-                angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))
-                if angle <= math.pi / 2:  # angle less than 90 degree, treat as fingers
-                    number_of_fingers += 1
-                    cv2.circle(canvas,far,10,[255,0,0],-1) #find the fingers
-            #the for loop puts the circles at the base of fingers
-
+            draw_dot_between_fingers(defects,largest_contour,canvas)
             fingerTip = farthest_point(defects, largest_contour, centre_of_hand)
             cv2.circle(canvas, fingerTip, 10, [255, 255, 0], -1) #fingertip is turquoise
-
             print("finger tip is in this format: X: %i Y: %i "%(fingerTip[0], fingerTip[1]))
             fingerTip_x = fingerTip[0]
             fingerTip_y = fingerTip[1]
             if user_currently_painting:
+                #pen_DOWN
                 if fingerTip_y <= 65: #if the user's fingertip is in the menu region
-                    if 40 <= fingerTip_x <= 140: # user selecting CLEAR ALL
+                    menu_button_pressed = what_menu_button_pressed(fingerTip_x)
+                    if menu_button_pressed == "CLEAR ALL": # user selecting CLEAR ALL
                         bpoints = [deque(maxlen=512)]
                         gpoints = [deque(maxlen=512)]
                         rpoints = [deque(maxlen=512)]
@@ -194,22 +217,16 @@ while True:
                         yindex = 0
 
                         paintWindow[67:,:,:] = 255 #make the whole canvas white
-                    elif 160 <= fingerTip_x <= 255:
-                            colorIndex = 0 # Blue
-                    elif 275 <= fingerTip_x <= 370:
-                            colorIndex = 1 # Green
-                    elif 390 <= fingerTip_x <= 485:
-                            colorIndex = 2 # Red
-                    elif 505 <= fingerTip_x <= 600:
-                            colorIndex = 3 # Yellow
+                    else:
+                        current_colour = menu_button_pressed
                 else :
-                    if colorIndex == 0:
+                    if current_colour == "BLUE":
                         bpoints[bindex].appendleft(fingerTip)
-                    elif colorIndex == 1:
+                    elif current_colour == "GREEN":
                         gpoints[gindex].appendleft(fingerTip)
-                    elif colorIndex == 2:
+                    elif current_colour == "RED":
                         rpoints[rindex].appendleft(fingerTip)
-                    elif colorIndex == 3:
+                    elif current_colour == "YELLOW":
                         ypoints[yindex].appendleft(fingerTip)
             else:
                 #pen_UP
@@ -222,16 +239,15 @@ while True:
                 ypoints.append(deque(maxlen=512))
                 yindex += 1
 
-
-            points = [bpoints, gpoints, rpoints, ypoints]
-            for i in range(len(points)):
-                for j in range(len(points[i])): #looping through the list of queues
-                    length_of_dequeList = len(points[i][j])
-                    for k in range(1, length_of_dequeList):#looping through the dequeList
-                        if points[i][j][k - 1] is None or points[i][j][k] is None:
+            #this triple nested for loop is to draw the current drawings
+            coloured_points = {"BLUE":bpoints, "GREEN":gpoints,"RED": rpoints, "YELLOW":ypoints}
+            for key in coloured_points:
+                for j in range(len(coloured_points[key])):
+                    length_of_dequeList = len(coloured_points[key][j])
+                    for k in range(1,length_of_dequeList):
+                        if coloured_points[key][j][k - 1] is None or coloured_points[key][j][k] is None:
                             continue
-                        #cv2.line(frame, points[i][j][k - 1], points[i][j][k], colors[i], 2)
-                        cv2.line(paintWindow, points[i][j][k - 1], points[i][j][k], colors[i], 2)
+                        cv2.line(paintWindow, coloured_points[key][j][k - 1], coloured_points[key][j][k], colours[key], 2)
     else:
         print("on the screen show some text that says PLACE A SINGLE HAND ON SCREEN")
 
@@ -244,9 +260,10 @@ while True:
     if k == 27:
         break
     if k == ord('p'):
-        print("user wants to toggle the user_currently_painting value")
+        #pen_up -> pen_down
+        #pen_down -> pen_up
         user_currently_painting = not(user_currently_painting)
 
 
 cv2.destroyAllWindows()
-cap.release()
+webcam.release()
